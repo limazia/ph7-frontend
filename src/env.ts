7ef3069c80933f46000684e0e63047b49d58f470
Acacio de Lima <limadeacacio@gmail.com>
2025-10-03 16:13:38 -0300

    feat: add environment schema validation using Zod

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+  MODE: z.enum(["development", "production"]).default("production"),
+
+  VITE_API_URL: z.string(),
+  VITE_ENABLE_API_DELAY: z.string().transform((value) => value === "true"),
+});
+
+export const env = envSchema.parse(import.meta.env);
