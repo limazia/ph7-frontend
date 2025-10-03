1a18a35160a6a7ad7fab31210962509dcdc995aa
Acacio de Lima <limadeacacio@gmail.com>
2025-10-03 16:16:01 -0300

    feat: add character schema interfaces for API response structure

---
+++ src/shared/schemas/character.schema.ts
@@ -0,0 +1,36 @@
+export interface Characters {
+  info: Info;
+  results: Character[];
+}
+
+interface Info {
+  count: number;
+  pages: number;
+  next: string;
+  prev: any;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Origin;
+  location: Location;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+interface Origin {
+  name: string;
+  url: string;
+}
+
+interface Location {
+  name: string;
+  url: string;
+}
