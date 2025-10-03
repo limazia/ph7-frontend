4247150c48eb2cc81dd3b1858d98ada057f34dac
Acacio de Lima <limadeacacio@gmail.com>
2025-10-03 16:13:50 -0300

    refactor: update main.tsx to use new App import and correct root element

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { App } from "@/app";
+
+const container = document.getElementById("app")!;
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
