c60c01ac26cef11856436d145be2f6a598d5555a
Acacio de Lima <limadeacacio@gmail.com>
2025-10-03 16:12:53 -0300

    feat: enhance Vite configuration with Tailwind CSS support and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
